b6f9d39aafa31627a8b4702f9ab0d39cfeea1774
Robert <robert-pelzer@gmx.net>
2026-05-12 15:58:51 +0200

    Add backend and frontend

--- src/main/java/com/transfermark/transfermarkt_analyst/exception/ClubNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.transfermark.transfermarkt_analyst.exception;
-
-public class ClubNotFoundException extends RuntimeException {
-    public ClubNotFoundException(String clubName) {
-        super("Verein '" + clubName + "' nicht gefunden");
-    }
-}
